eb4cd391b16215baddcbfc25b321b4a24e5959b7
Dmitry <dmitrio.skripka@gmail.com>
2021-11-11 01:43:18 +0300

    2gis 1.0.0

--- two_gis_scrap.py
+++ two_gis_scrap.py
@@ -1,34 +1,37 @@
 from aiohttp import ClientSession
 import asyncio
-import aiohttp
 from bs4 import BeautifulSoup
-from numpy import short
+from datetime import datetime, timedelta
 import pandas as pd
+import lxml
 import os
+import re
+import requests
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
-
 from seleniumwire import webdriver
 import time
 
-town_pairs = {
-    'VOLGOGRAD': 'https://2gis.ru/volgograd/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D1%8F?m=44.428131%2C48.653067%2F11',
-    'VORONEZH': 'https://2gis.ru/voronezh/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=39.200594%2C51.66073%2F11',
-    'KRASNODAR': 'https://2gis.ru/krasnodar/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=38.971526%2C45.024359%2F11',
-    'EKATERINBURG': 'https://2gis.ru/ekaterinburg/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=60.61642%2C56.839656%2F11',
-    'CHELYABINSK': 'https://2gis.ru/chelyabinsk/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=61.402709%2C55.157389%2F11',
-    'NIZNIYNOVG': 'https://2gis.ru/n_novgorod/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=43.928022%2C56.301157%2F11',
-    'SAMARA': 'https://2gis.ru/samara/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=50.180997%2C53.20698%2F12',
-    'KAZAN': 'https://2gis.ru/kazan/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=49.130025%2C55.798875%2F11',
-    'UFA': 'https://2gis.ru/ufa/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=56.023289%2C54.773241%2F11',
-    'SARATOV': 'https://2gis.ru/saratov/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=46.034329%2C51.533296%2F11',
-    'YOSHKAR-OLA': 'https://2gis.ru/yoshkarola/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=47.887645%2C56.637319%2F11',
-    'RND': 'https://2gis.ru/rostov/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=39.691771%2C47.222099%2F11',
-    'MSC': 'https://2gis.ru/moscow/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=37.62017%2C55.753466%2F11',
-    'SPB': 'https://2gis.ru/spb/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=30.321941%2C59.928527%2F11', 
-}
+
+headers = {
+       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebt/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36' 
+    }
+URL_PART_1 = 'https://api.reviews.2gis.com/2.0/branches/'
+URL_PART_2 = '/reviews?fields=meta.providers%2Cmeta.branch_rating%2Cmeta.branch_reviews_count%2Cmeta.total_count%2Creviews.hiding_reason%2Creviews.is_verified&is_advertiser=false&key=37c04fe6-a560-4549-b459-02309cf643ad&limit=50&locale=ru_RU&offset_date='
+URL_PART_3 = 'T12%3A27%3A50.824879%2B07%3A00&rated=true&sort_by=date_edited'
+TOWN_PAIRS = {
+    'PERM': 'https://2gis.ru/perm/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=56.248179%2C58.007079%2F11',
+    'KRASNOYARSK': 'https://2gis.ru/krasnoyarsk/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=92.830745%2C56.005498%2F11',
+    'NEFTEYUGANSK': 'https://2gis.ru/nefteyugansk/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=72.615349%2C61.068648%2F11.55',
+    'HANTI-MANSIYSK': 'https://2gis.ru/kh_mansiysk/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=69.053016%2C60.998366%2F13.34',
+    'NIZNEVERTOVSK': 'https://2gis.ru/nizhnevartovsk/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D0%B5?m=76.572051%2C60.933784%2F11',
+    }
+
 
 def get_grefs(city_pairs: dict) -> None:
+    """
+    Base data scraping, get companys URLS and save them to file with path 2gis\\row_hrefs\\
+    """
 
     login = 'dnxfuf'
     password = '24lhnBJG9a'
@@ -47,13 +50,11 @@ def get_grefs(city_pairs: dict) -> None:
     for city in cities:
         hrefs = []
         try:
-            i = 1
+            page_number = 1
             driver.get(city_pairs[city])
-            # driver.get('https://2gis.ru/volgograd/search/%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D1%8F?m=44.428131%2C48.653067%2F11')
             while True:
-                
                 try:  
-                    i+=1
+                    page_number += 1
                     element = driver.find_element(by='class name', value='_1x4k6z7')
                     ActionChains(driver).move_to_element(element).perform()
                     time.sleep(3)
@@ -61,9 +62,10 @@ def get_grefs(city_pairs: dict) -> None:
                     for each in soup.find_all('div', class_='_1hf7139'):
                         url = each.find('div', class_='_1h3cgic').find('a').get('href')
                         hrefs.append(url)
-                    page = driver.find_element(by='xpath', value = f"//span[text()='{i}']")
+                    # Ищем номер страницы, если он будет отсутствовать, тогда записываем полученные данные по городу в файл
+                    page = driver.find_element(by='xpath', value = f"//span[text()='{page_number}']")
                     page.click()
-                    time.sleep(2)              
+                    time.sleep(2)
 
                 except NoSuchElementException:
                     with open(f'2gis\\row_hrefs\\{city}.txt', 'w', encoding='utf8') as file:
@@ -73,9 +75,10 @@ def get_grefs(city_pairs: dict) -> None:
         except Exception as _ex:
             print(_ex)
 
+
 def prepare_hrefs(city_dict):
     '''
-    Создание полных ссылок, для дальнейшей работы
+    Create full url links of each company
     '''
 
     cities = [city for city in city_dict.keys()]
@@ -89,38 +92,44 @@ def prepare_hrefs(city_dict):
         print(f'Ссылки по городу {city} были обработаны.')
 
 
-async def gather_base_info(file_path:str):
-
-    global short_data
-    short_data = []
+async def gather_data(file_path):
+    """
+    Create massiv of tasks to get base data about company
+    """
     tasks = []
     for file in os.listdir(file_path):
         city = file.split('.')[0]
-
+        print(city)
         with open(os.path.join(file_path, file), encoding='utf8') as f:
-            urls = [url.strip() for url in f.readlines()]     
-            lenght = len(urls)
-            
-            for pos, url in enumerate(urls):                   
-                task = asyncio.create_task(get_base_info(url, city, pos, lenght))
+            urls = [url.strip() for url in f.readlines()]
+            # print(urls)
+            for pos, url in enumerate(urls):
+                # print(url)
+                task = asyncio.create_task(get_base_data(url, city, pos, len(urls)))
                 tasks.append(task)
-        # pd.DataFrame(short_data, columns=['City', 'Company', 'Phone', 'URL', 'Social new']).to_csv(f'2gis\\company_info\\{city}.csv', index=False)
-        # await asyncio.sleep(20)
-        # pd.DataFrame(short_data, columns=['City', 'Company', 'Phone', 'URL', 'Social new']).to_csv(f'2gis\\company_info\\CUMUL-{city}.csv', index=False)
-    await asyncio.gather(*tasks)
+        # print(f'Обработано {counter+1}/{TOTAL_FILES} файл(а/ов)')
+        await asyncio.sleep(30)
+    asyncio.gather(*tasks)
 
 
-async def get_base_info(url: str, city: str, pos, lenght):
-    '''
-    Функция запросов и создание соответствующейго csv под файл
-    '''
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
-
-    # length = len(urls)
-    # data = []
-    async with ClientSession() as session:        
-        response = await session.get(url=url, headers=headers)
-        soup = BeautifulSoup(await response.text(), 'lxml')
+async def get_base_data(url, city, pos, lenght):
+    """
+    Function parse data from each url and take base info like company_id, company_name, phone, url, social_media
+    """
+    headers = {
+       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebt/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36' 
+    }
+    # proxy = {'http':'103.95.40.211:3128'}
+    async with ClientSession() as session:
+        try:
+            response = await session.get(url=url, headers=headers)
+            soup = BeautifulSoup(await response.text(), 'lxml')
+        except Exception as _ex:
+            print(_ex)
+        try:
+            company_id = url.split('firm/')[1]
+        except:
+            company_id = url.split('geo/')[1]
         try:
             company_name = soup.find('span', class_='_oqoid').text
         except:
@@ -130,40 +139,128 @@ async def get_base_info(url: str, city: str, pos, lenght):
         except AttributeError:
             phone = 'Неизвестно'
         try:
-            url = soup.find('div', class_='_49kxlr').find('a', class_='_1rehek').text
+            url = soup.find_all('a', {'target': '_blank', 'class':'_1rehek'})[1]
+            if '.ru' or '.com' not in url:
+                url = 'Неизветно'
         except AttributeError:
             url = 'Неизвестно'
         try:
             social_media = '\n'.join([url.get('href') for url in soup.find_all('a', class_='_1rehek')[:4]])
         except AttributeError:
             social_media = 'Неизвестно'
-        short_data.append([city, company_name, phone, url, social_media])
+        short_data.append([city, company_name, company_id, phone, url, social_media])
         print(f'Обработано: {pos}/{lenght}')
 
-def concat_pandas(file_path: str):
 
-    files = os.listdir(file_path)[8:]
+def format_hrefs(file_path):
+    '''
+    Делаем URLSы для публичной апишки, через которую будут запросы
+    '''
+    
+    for file in os.listdir(file_path):
+        urls = []
+        with open(os.path.join(file_path, file), encoding='utf8') as f:
+            # Забираем id компании            
+            for url in f.readlines():
+                try:
+                    urls.append(url.strip().split('firm/')[1])
+                except IndexError:
+                    continue               
+        
+        with open(f'2gis\\review_hrefs\\{file}', 'w', encoding='utf8') as f_result:
+            for url in urls:
+                # link = URL_PART_1 + url + URL_PART_2 + datetime.today().strftime('%Y-%m-%d') + URL_PART_3 + '\n'     
+                f_result.write(url+"\n")
+
+        print(f'Ссылки по городу {file[:-4]} были подготовлены')
+
+
+def company_total_data():
+    """
+    Concat dataframes to single frame
+    """
     df = pd.DataFrame()
+    for file in os.listdir(f'2gis/company_info'):
+        try:
+            new = pd.read_csv(f'2gis/company_info/{file}')
+        except:
+            continue
+        new['City'] = file.split('.')[0]
+        # print(new)
+        df = pd.concat([df, new])
+    
+    df = df.loc[:, ['City', 'text', 'rating', 'date_created', 'user', 'object', 'official_answer']]
+    df = df.reset_index(drop=True)
+    df['date_created'] = df['date_created'].str[:10]
+    df['Comment author'] = df['user'].apply(lambda x: re.findall("'name': '(.+?)', 'p", x))
+    df['Company_id'] = df['object'].apply(lambda x: re.findall("'id': '(.+?)', 'typ", x)[0])
+    df = df.drop(columns=['user', 'object', 'official_answer'])
+    df2 = pd.read_csv('2gis/ALL_DATA/company_base_info_2gis.csv')
+    df2 = df2.loc[:, ['Company', 'Company_id']]
+    df['Company_id'] = df['Company_id'].astype('int64')
+    df2['Company_id'] = df2['Company_id'].astype('int64')
+    df = df.merge(df2, how='left', on='Company_id')
+    df = df.loc[:, ['City', 'Company', 'Company_id', 'text', 'date_created', 'rating', 'Comment author']]
+    df.drop_duplicates(subset=['text', 'date_created'], inplace=True)
 
-    for file in files:
-        df_city = pd.read_csv(os.path.join(file_path, file))
-        df = pd.concat([df, df_city])
-    df.drop_duplicates(inplace=True)
-    df.to_csv('2gis\\ALL_DATA\\basic.csv', index=False)
+    return df
+
+
+def total_data_requests():
+    """
+    Send requests to get needed data and save it to csv
+    """
+
+    for city_file in os.listdir('2gis/review_hrefs'):
+        city = city_file.split('.')[0]
+        df = pd.DataFrame()
+        with open(os.path.join('2gis/review_hrefs', city_file)) as file:
+            urls_id = [url.strip() for url in file.readlines()]
+        with requests.Session() as session:
+    #         ПО одному городу
+            for url_id in urls_id:
+                date = str(datetime.now().date() + timedelta(days=1))
+                offset = ''
+                while True:
+                    url = URL_PART_1 + url_id + URL_PART_2 + date + URL_PART_3
+                    # print(url)
+                    response = session.get(url=url, headers=headers)
+                    js = response.json()['reviews']
+                    
+                    try:
+                        date = js[-1]['date_created'][:10]
+                        offset_now = js[0]['id']
+                        if offset_now == offset:
+                            break
+                        # offset: last data in review
+                        offset = offset_now
+                    except IndexError:
+                        break
+                    new = pd.DataFrame().from_dict(js)
+                    df = pd.concat([df,new])
+                    if len(js) == 1:
+                        break
+            df.to_csv(f'2gis\\company_info\\{city}.csv', index=False)
+            
+
 
 def main():
-    # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(gather_base_info('2gis\\hrefs\\'))
-    pd.DataFrame(short_data, columns=['City', 'Company', 'Phone', 'URL', 'Social new']).to_csv('2gis\\ALL_DATA\\BASE_SUMARY_2gis.csv', index=False)
-    # concat_pandas('2gis\\company_info\\')
-    # global data
-    # data = []
-    # get_grefs(town_pairs)
-    # prepare_hrefs(town_pairs)
-    # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    # asyncio.run(gather_data()) # Передать словарь
-    # pd.DataFrame(data, columns=['City', 'URL']).to_csv(os.path.join('ALL_goog.csv'), index=False)
-    # pd.DataFrame(reviews_info, columns=['Company', 'City', 'Name', 'Date', 'Review']).to_csv(os.path.join('ALL.csv'), index=False)
-    
+    """
+    The main goal of this scraper get reviews, reviews date, author of review
+    Right order to achive a result. Run functions is following order: get_grefs -> prepare_hrefs -> gather_data -> total_data_requests ->
+    company_total_data
+    """
+    global short_data
+    short_data = []
+
+    get_grefs(TOWN_PAIRS)
+    prepare_hrefs(TOWN_PAIRS)
+    asyncio.run(gather_data('2gis\\hrefs\\'))
+    pd.DataFrame(short_data, columns=['City', 'Company', 'Company_id', 'Phone', 'URL', 'Social new']).to_csv('2gis\\ALL_DATA\\company_base_info_2gis.csv', index=False)
+    format_hrefs('2gis\\hrefs\\')
+    total_data_requests()
+    company_total_data()
+    print('Обработка отзывов была полностью завершена.')
+
 if __name__ == '__main__':
     main()
